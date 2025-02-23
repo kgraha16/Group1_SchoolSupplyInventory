245ee3f92174d260f39810a7e373f5979782d41e
Kris Graham <kgraha16@asu.edu>
2025-02-22 20:17:24 -0700

    Add project structure

---
+++ src/main/java/SchoolSupplyInventory/database/jdbc.java
@@ -0,0 +1,7 @@
+package main.java.SchoolSupplyInventory.database;
+
+public class jdbc {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/SchoolSupplyInventory/TestUserInterface.java
@@ -0,0 +1,5 @@
+package test.java.SchoolSupplyInventory;
+
+public class TestUserInterface {
+
+}
